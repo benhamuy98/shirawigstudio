53710939c536bf6cc66425f6f843513877820aec
Yehuda <benhamuy@gmail.com>
2026-06-29 19:57:35 +0300

    Add service worker for automatic updates on iOS/Android PWA

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'studio-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  // Network-first: always try to get fresh content, fall back to cache
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
